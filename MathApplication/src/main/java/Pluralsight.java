85f48ba374d96b575010873e0e9879b1f52df43b
Jaleel <jaleel.pearson96@gmail.com>
2024-04-11 13:44:02 -0400

    completed Mod3 Exercise 2 -MathApp

---
+++ MathApplication/src/main/java/Pluralsight.java
@@ -0,0 +1,37 @@
+public class MathApp {
+    public static void main(String[] args) { int BobSalary = 10000;
+        int GarySalary = 50000;
+    }
+
+// Question 1:
+ int BobSalary = 10000;
+ int GarySalary = 50000;
+ int highestSalary = Math.max(BobSalary, GarySalary);
+ System.out.println("The higher salary is: " + GarySalary);
+
+
+
+
+ Question 2:
+int CarPrice = 3000
+ int TruckPrice = 6000
+ int LowestPrice = Math.max(CarPrice, TruckPrice);
+ System.out.println(The lowest price is 3000 + LowestPrice)
+
+
+
+
+ //Question 3:
+ double radius = 7.25
+ double area = Math.PI * Math.pow(radius, 2);
+System.out.println("The area of the circle with radius " + radius + " is approximately " + area + " square units.");
+    }
+//}
+
+
+//Question 4
+/double number = 5.0;
+double squareRoot = Math.sqrt(number);
+System.out.println("The square root of " + number + " is approximately " + squareRoot);
+}
+}
